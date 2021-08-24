7ba8c9d36fea6eba7d492cbe22d073f7a4412c87
dqwdd <dqwdd@naver.com>
2021-08-24 10:07:47 +0900

    프래그먼트의 로그 버튼 동작 생성

--- app/src/main/java/com/example/fregment_2021_08_24_home/fragments/FirstFragment.kt
+++ app/src/main/java/com/example/fregment_2021_08_24_home/fragments/FirstFragment.kt
@@ -1,11 +1,14 @@
 package com.example.fregment_2021_08_24_home.fragments
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.fregment_2021_08_24_home.R
+import kotlinx.android.synthetic.main.fragment_first.*
 
 class FirstFragment : Fragment() {
 
@@ -18,9 +21,20 @@ class FirstFragment : Fragment() {
         container : ViewGroup?,
         savedInstanceState: Bundle?
     ) : View? {
-        return inflater.inflate(R.layout.fragment_first), container, false)
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
 
+//    동작 관련 코드들이 작성되는 함수 => 액티비티 : onCreate
+//    이 클래스는 화면이 만들어진게 아니라 올라간 부품이 만들어 진거임
+//    먼저 만들어 놓고 그 다음에 올리는 거임
+//    ==> 프래그먼트의 onCreate는 화면도 준비되기 전 -> 동작시키면 에러 소지가 높다
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        logBtn.setOnCreateContextMenuListener(
+            Log.d("부품에서 찍기", "로그 버튼이 눌림")
+        )
     }
 
 }
